724d887efe743d2db9f1035d9a0086de4e33cc62
Lilang <772576761@qq.com>
2022-05-23 00:19:19 +0800

    the next day

--- src/main.go
+++ src/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	_ "Once/src/hello"
+	"fmt"
+	"os"
 )
 
 func main() {
 	// 如果要使用hello包中的Print函数，则把_去掉
+	// 下划线在代码中的使用 - 忽略返回的参数
+	buf := make([]byte, 1024)
+	f, _ := os.Open("./test.txt")
+	defer f.Close()
+	for {
+		n, _ := f.Read(buf)
+		if n == 0 {
+			fmt.Println("file is not exsit")
+			break
+		}
+		os.Stdout.Write(buf[:n])
+	}
 }
