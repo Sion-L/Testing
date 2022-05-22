4268f8eadc6117b0773f9b02b396b5809b6a441b
Lilang <772576761@qq.com>
2022-05-23 00:13:34 +0800

    the next day

---
+++ src/hello/hello.go
@@ -0,0 +1,11 @@
+package hello
+
+import "fmt"
+
+func init() {
+	fmt.Println("import init")
+}
+
+func Print() {
+	fmt.Println("hello")
+}
---
+++ src/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	_ "Once/src/hello"
+)
+
+func main() {
+	// 如果要使用hello包中的Print函数，则把_去掉
+}
